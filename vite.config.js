192d8d93dbd0287b90dad8e9b822fff428e6cdeb
SullyInaHoodie <112969869+SullyInaHoodie@users.noreply.github.com>
2024-06-07 19:54:45 -0500

    fixed everything

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/free-solid-svg-icons'],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       external: ['@fortawesome/free-solid-svg-icons']
+  //     }
+  //   }
+  // }
 });
+
+
+
